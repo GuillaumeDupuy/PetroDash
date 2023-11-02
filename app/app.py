65e97f5ea8a7c7e51e84aaae8bf11569e944f3ec
Guillaume <guillaume.dupuy@ynov.com>
2023-11-02 11:00:05 +0100

    update data & add image map

--- app/app.py
+++ app/app.py
@@ -291,22 +291,27 @@ def generate_map(data):
                     <b>Address</b>: {adresse}<br/>
                     <b>City</b>: {cp} {ville}<br/>
                     <b>Brand</b>: {brand}<br/>
-                    <b>Image brand</b>: <img src="https://raw.githubusercontent.com/GuillaumeDupuy/PetroDash/main/image/brands/{brand_logo}.png" width="50" height="50"><br/>
+                    <b>Image brand</b>: <img src="https://raw.githubusercontent.com/GuillaumeDupuy/PetroDash/main/image/brands/{brand_logo}.png" width="30" height="30"><br/>
                 </div>
                 <div style="flex: 1;">
+                    <b> Image Gazole</b>: <img src="https://raw.githubusercontent.com/GuillaumeDupuy/PetroDash/main/image/fuels/b7.png" width="30" height="30"><br/>
                     <b>Price Gazole</b>: {gazole_prix} €<br/>
                     <b>Update on</b>: {gazole_maj}<br/>
+                    <b> Image SP98</b>: <img src="https://raw.githubusercontent.com/GuillaumeDupuy/PetroDash/main/image/fuels/e5.png" width="30" height="30"><br/>
                     <b>Price SP98</b>: {sp98_prix} €<br/>
                     <b>Update on</b>: {sp98_maj}<br/>
+                    <b> Image E85</b>: <img src="https://raw.githubusercontent.com/GuillaumeDupuy/PetroDash/main/image/fuels/e85.png" width="30" height="30"><br/>
                     <b>Price E85</b>: {e85_prix} €<br/>
                     <b>Update on</b>: {e85_maj}<br/>
                 </div>
                 <div style="flex: 1;">
-                    
+                    <b> Image SP95</b>: <img src="https://raw.githubusercontent.com/GuillaumeDupuy/PetroDash/main/image/fuels/e10.png" width="30" height="30"><br/>
                     <b>Price SP95</b>: {sp95_prix} €<br/>
-                    <b>Update on</b>: {sp95_maj}<br/>  
+                    <b>Update on</b>: {sp95_maj}<br/> 
+                    <b> Image E10</b>: <img src="https://raw.githubusercontent.com/GuillaumeDupuy/PetroDash/main/image/fuels/e10.png" width="30" height="30"><br/>
                     <b>Price E10</b>: {e10_prix} €<br/>
                     <b>Update on</b>: {e10_maj}<br/>
+                    <b> Image GPLc</b>: <img src="https://raw.githubusercontent.com/GuillaumeDupuy/PetroDash/main/image/fuels/lpg.png" width="30" height="30"><br/>
                     <b>Price GPLc</b>: {gplc_prix} €<br/>
                     <b>Update on</b>: {gplc_maj}<br/>
                 </div>
